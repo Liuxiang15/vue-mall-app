30ab8c36f6ef1fcfc30931636f39d358ec35f774
liuxiang7 <liuxiang7@tal.com>
2022-02-20 21:59:07 +0800

    feat:顶部搜索框

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,8 @@
 <template>
   <div id="app">
-    <van-button type="primary">主要按钮</van-button>
+    <router-view></router-view>
   </div>
 </template>
 
 <style lang="less">
-#app {
-  width: 750px;
-  height: 100px;
-  // background-color: #f00;
-}
 </style>
---
+++ src/views/Classify.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="classify-wrapper">
+    <div class="search-btn">
+      <van-icon name="search"></van-icon>
+      <div>苹果特价一元一斤</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+};
+</script>
+
+<style lang="less">
+.classify-wrapper {
+  width: 375px;
+  .search-btn {
+    width: 355px;
+    height: 33px;
+    // 垂直居中
+    line-height: 33px;
+    margin: 11px auto;
+    border-radius: 10px;
+    background-color: #eee;
+    font-size: 14px;
+    text-align: center;
+    color: #a1a1a1;
+    > * {
+      display: inline-block;
+      vertical-align: middle;
+    }
+  }
+}
+</style>
